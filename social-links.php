e2c365ea960132ebfe9afaf3de5af45a0bd158d4
Chris <chris@codecomposer.co.uk>
2015-02-20 23:26:56 +0000

    Social Links update

--- social-links.php
+++ social-links.php
@@ -1,19 +1,27 @@
 <ul class="social-links">
 	<?php if( $id = get_field('facebook_id', 'option') ): ?>
-	<li class="facebook">
-		<a href="http://facebook.com/<?php echo $id; ?>" title="Find us on Facebook"><?php if( wp_script_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-facebook"></i><?php else: ?>Find us on Facebook<?php endif; ?></a>
-	</li>
+		<li class="facebook">
+			<a href="http://facebook.com/<?php echo $id; ?>" title="Find us on Facebook"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-facebook"></i><?php else: ?>Find us on Facebook<?php endif; ?></a>
+		</li>
 	<?php endif; ?>
 	<?php if( $id = get_field('twitter_id', 'option') ): ?>
 		<li class="twitter">
-			<a href="http://twitter.com/<?php echo $id; ?>" title="Find us on Twitter"><?php if( wp_script_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-twitter"></i><?php else: ?>Find us on Twitter<?php endif; ?></a>
+			<a href="http://twitter.com/<?php echo $id; ?>" title="Find us on Twitter"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-twitter"></i><?php else: ?>Find us on Twitter<?php endif; ?></a>
 		</li>
 	<?php endif; ?>
-	<?php if( $id = get_field('googleplus_id', 'option') ): ?>
+	<?php if( $id = get_field('google_plus_id', 'option') ): ?>
 		<li class="google">
-			<a href="https://plus.google.com/u/0/<?php echo $id; ?>/posts" title="Find us on Google+"><?php if( wp_script_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-google-plus"></i><?php else: ?>Find us on Google+<?php endif; ?></a>
+			<a href="https://plus.google.com/<?php echo $id; ?>/posts" title="Find us on Google+"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-google-plus"></i><?php else: ?>Find us on Google+<?php endif; ?></a>
 		</li>
 	<?php endif; ?>
+	<?php if( $id = get_field('pinterest_id', 'option') ): ?>
+		<li class="pinterest">
+			<a href="https://uk.pinterest.com/<?php echo $id; ?>" title="Find us on Pinterest"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-pinterest"></i><?php else: ?>Find us on Pinterest<?php endif; ?></a>
+		</li>
+	<?php endif; ?>
+	<li class="rss">
+		<a href="/feed/" title="View our feed"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-rss"></i><?php else: ?>View our RSS feed<?php endif; ?></a>
+	</li>
 	<?php
 	//TODO Add other social outlets
 	?>
