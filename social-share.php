a04b7c5214040de8127ea060a1dca79ff1b37739
Chris <chris@codecomposer.co.uk>
2015-04-16 22:33:10 +0100

    Font Awesome Enque fix

--- social-share.php
+++ social-share.php
@@ -18,15 +18,15 @@ if( is_home() ){
 
 <ul class="social-share">
 	<li>
-		<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>" title="Share on Facebook"><?php if( wp_script_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-facebook"></i><?php else: ?>Share on Facebook<?php endif; ?></a>
+		<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>" title="Share on Facebook"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-facebook"></i><?php else: ?>Share on Facebook<?php endif; ?></a>
 	</li>
 	<li>
-		<a href="https://twitter.com/home?status=<?php echo $url; ?> <?php echo $title; ?>" title="Share on Twitter"><?php if( wp_script_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-twitter"></i><?php else: ?>Share on Twitter<?php endif; ?></a>
+		<a href="https://twitter.com/home?status=<?php echo $url; ?> <?php echo $title; ?>" title="Share on Twitter"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-twitter"></i><?php else: ?>Share on Twitter<?php endif; ?></a>
 	</li>
 	<li>
-		<a href="https://plus.google.com/share?url=<?php echo $url; ?>" title="Share on Google plus"><?php if( wp_script_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-google-plus"></i><?php else: ?>Share on google+<?php endif; ?></a>
+		<a href="https://plus.google.com/share?url=<?php echo $url; ?>" title="Share on Google plus"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-google-plus"></i><?php else: ?>Share on google+<?php endif; ?></a>
 	</li>
 	<li>
-		<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $url; ?>&title=<?php echo $title; ?>&summary=&source="<?php echo get_site_url(); ?> title="Share on LinkedIn"><?php if( wp_script_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-linkedin"></i><?php else: ?>Share on LinkedIn<?php endif; ?></a>
+		<a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $url; ?>&title=<?php echo $title; ?>&summary=&source="<?php echo get_site_url(); ?> title="Share on LinkedIn"><?php if( wp_style_is( 'font-awesome', 'enqueued' ) ):?><i class="fa fa-linkedin"></i><?php else: ?>Share on LinkedIn<?php endif; ?></a>
 	</li>
 </ul>
