c877ddbcb422ce4014f1bbf7c9bfae3a02c663eb
Chris <chris@codecomposer.co.uk>
2015-04-17 00:27:41 +0100

    Added browser detection

--- functions-theme.php
+++ functions-theme.php
@@ -107,3 +107,20 @@ function add_current_nav_class($classes, $item)
 	return $classes;
 }
 add_action('nav_menu_css_class', 'add_current_nav_class', 10, 2 );
+
+function browser_body_class($classes) {
+	global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+
+	if($is_lynx) $classes[] = 'lynx';
+	elseif($is_gecko) $classes[] = 'gecko';
+	elseif($is_opera) $classes[] = 'opera';
+	elseif($is_NS4) $classes[] = 'ns4';
+	elseif($is_safari) $classes[] = 'safari';
+	elseif($is_chrome) $classes[] = 'chrome';
+	elseif($is_IE) $classes[] = 'ie';
+	else $classes[] = 'unknown';
+
+	if($is_iphone) $classes[] = 'iphone';
+	return $classes;
+}
+add_filter('body_class','browser_body_class');
